2ba3fcc8cbf9431ab714fe00e1ad703c6fb6e215
jwsolve <jwsolve@gmail.com>
2014-11-22 15:16:39 +0000

    Initial build 0.01

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,100 @@
+######################################################################################
+#
+#	Zumvo.com - v0.01
+#
+######################################################################################
+
+TITLE = "Zumvo.com"
+PREFIX = "/video/zumvo"
+ART = "art-default.jpg"
+ICON = "icon-default.png"
+ICON_LIST = "icon-list.png"
+ICON_COVER = "icon-cover.png"
+ICON_SEARCH = "icon-search.png"
+ICON_NEXT = "icon-next.png"
+ICON_MOVIES = "icon-movies.png"
+ICON_SERIES = "icon-series.png"
+ICON_QUEUE = "icon-queue.png"
+BASE_URL = "http://zumvo.com"
+MOVIES_URL = "http://zumvo.com/movies"
+
+######################################################################################
+# Set global variables
+
+def Start():
+
+	ObjectContainer.title1 = TITLE
+	ObjectContainer.art = R(ART)
+	DirectoryObject.thumb = R(ICON_COVER)
+	DirectoryObject.art = R(ART)
+	VideoClipObject.thumb = R(ICON_COVER)
+	VideoClipObject.art = R(ART)
+	
+	HTTP.CacheTime = CACHE_1HOUR
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'
+	HTTP.Headers['Referer'] = 'http://zumvo.com/'
+	
+######################################################################################
+# Menu hierarchy
+
+@handler(PREFIX, TITLE, art=ART, thumb=ICON)
+def MainMenu():
+
+	oc = ObjectContainer()
+	oc.add(DirectoryObject(key = Callback(ShowCategory, title="Movies", category="movies", page_count = 1), title = "Movies", thumb = R(ICON_MOVIES)))
+	
+	return oc
+
+######################################################################################
+# Creates page url from category and creates objects from that page
+
+@route(PREFIX + "/showcategory")	
+def ShowCategory(title, category, page_count):
+
+	oc = ObjectContainer(title1 = title)
+	page_data = HTML.ElementFromURL(MOVIES_URL + '/page/' + str(page_count) + '/')
+	
+	for each in page_data.xpath("//ul[@class='list-film']/li"):
+		url = each.xpath("./div[@class='inner']/a/@href")[0]
+		title = each.xpath("./div[@class='inner']/a/@title")[0]
+		thumb = each.xpath("./div[@class='inner']/a/img/@src")[0]
+		
+		oc.add(DirectoryObject(
+			key = Callback(EpisodeDetail, title = title, url = url),
+			title = title,
+			thumb = Resource.ContentsOfURLWithFallback(url = thumb, fallback='icon-cover.png')
+			)
+		)
+
+	oc.add(NextPageObject(
+		key = Callback(ShowCategory, title = title, page_count = int(page_count) + 1),
+		title = "More...",
+		thumb = R(ICON_NEXT)
+			)
+		)
+	
+	return oc
+
+######################################################################################
+# Gets metadata and google docs link from episode page. Checks for trailer availablity.
+
+@route(PREFIX + "/episodedetail")
+def EpisodeDetail(title, url):
+	
+	oc = ObjectContainer(title1 = title)
+	page_data = HTML.ElementFromURL(url)
+	title = page_data.xpath("//head/meta[@property='og:title']")[0]
+	thumb = page_data.xpath("//div[@class='poster']/a/img/@src")[0]
+
+	#load recursive iframes to find google docs url
+	first_frame_url = page_data.xpath("//a[@class='btn-watch']/@href")[0]
+	
+	oc.add(VideoClipObject(
+		url = first_frame_url,
+		title = title,
+		thumb = Resource.ContentsOfURLWithFallback(url = thumb, fallback='icon-cover.png')
+		)
+	)	
+	
+	return oc	
+	
